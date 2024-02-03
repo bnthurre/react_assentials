16e7459a0b0e504a15bde96547e958f07444a608
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-03 12:18:21 +0300

    using and outputting dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+const reactConcepts =["Fundamental", "Core","Crucial"];
+function getRamdon(max){
+  return Math.floor(Math.random()* (max +1));
+}
 function Header() {
+  const desc = reactConcepts[getRamdon(2)];
   return (
     <header>
       <img src="src/assets/reactc.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {desc} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
