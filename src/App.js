c3ab57fdf6980a6247d0db96e937b9f6797b4e69
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-03 13:34:00 +0300

    making components reusable with props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import reactImg from './assets/reactc.png'
+import reactImg from "./assets/reactc.png";
+import componentImg from "./assets/components.png";
 
-const reactConcepts =["Fundamental", "Core","Crucial"];
-function getRamdon(max){
-  return Math.floor(Math.random()* (max +1));
+const reactConcepts = ["Fundamental", "Core", "Crucial"];
+function getRamdon(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
   const desc = reactConcepts[getRamdon(2)];
@@ -11,10 +12,20 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {desc} React concepts you will need for almost any app you are
-        going to build!
+        {desc} React concepts you will need for almost any app you are going to
+        build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.decription}</p>
+    </li>
   );
 }
 function App() {
@@ -22,7 +33,19 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept
+              title="components"
+              decription="the core ui building blocks"
+              img={componentImg}
+            ></CoreConcept>
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
